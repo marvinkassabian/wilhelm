90340c09158214787cf14160f3d391350b4692a5
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-10-07 18:41:51 +0000

    Reduce script.js

--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-(function(global) {
-  "use strict";
+function Pair(m, n) {
+  this.m = m;
+  this.n = n;
+}
 
-  var WILHELM = (function() {
+function step(m, n) {
+  return step(new Pair(m, n));
+}
 
-    function namespace() {
-      var a = arguments;
-      var o = null;
-      var i, j, d;
+function step(pair) {
 
-      for (i = 0; i < a.length; i = i + 1) {
-        d = a[i].split('.');
-        o = WILHELM;
+}
 
-        for (j = ((d[0] === 'WILHELM') ? 1 : 0); j < d.length; j++) {
-          o[d[j]] = o[d[j]] || {};
-          o = o[d[j]];
-        }
-      }
+var first = new Pair(1,2);
 
-      return o;
-    }
-
-    this.namespace = namespace;
-
-    return this;
-  }).call(WILHELM || {});
-
-  global.WILHELM = WILHELM;
-})(this);
-
-(function() {
-  "use strict";
-
-  WILHELM.namespace('WILHELM.Pair');
-
-  WILHELM.Pair = (function(module) {
-
-    function Pair(m, n) {
-      this.m = m;
-      this.n = n;
-    }
-
-    function step(m, n) {
-      return step(new Pair(m, n));
-    }
-
-    function step(pair) {
-      
-    }
-
-    return module;
-  })(WILHELM.Pair);
-})();
+console.log(first);
