3d1e4f8c48413f10df1ca05b99804e98ad69e186
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-10-13 12:21:57 -0400

    Use different data structure

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,6 +6,71 @@
     this.list = [m, n];
   };
 
+  Ackermann.prototype.step = function() {
+    var m;
+    var n;
+    if (this.list.length > 1) {
+      m = this.list[this.list.length - 2];
+      n = this.list[this.list.length - 1];
+
+      this.list.pop();
+      this.list.pop();
+
+      if (m === 0) {
+        this.list.push(n + 1);
+      } else if (m > 0 && n === 0) {
+        this.list.push(m - 1);
+        this.list.push(1);
+      } else if (m > 0 && n > 0) {
+        this.list.push(m - 1);
+        this.list.push(m);
+        this.list.push(n - 1);
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  Ackermann.prototype.toString = function() {
+    return WILHELM.toString(this.list);
+  };
+
+  WILHELM.toString = function(list) {
+
+    var verbose = false;
+
+    var COMMA = verbose ? ', ' : ',';
+    var OPEN = verbose ? 'A(' : 'A';
+    var CLOSE = verbose ? ')' : '';
+
+    if (list.length === 1) {
+      return _.first(list);
+    } else {
+      return OPEN + _.first(list) + COMMA + WILHELM.toString(_.tail(list)) + CLOSE;
+    }
+  };
+
+  var a = new Ackermann(3);
+  var stepNotDone = true;
+  /*
+  do {
+    console.log(a.toString());
+    stepNotDone = a.step();
+  } while (stepNotDone);
+  */
+
+  var func = function() {
+    console.log(a.toString());
+    stepNotDone = a.step();
+    if (stepNotDone) {
+      setTimeout(func, 5);
+    }
+  };
+
+  func();
+
   /*
   var Pair = WILHELM.Pair.Pair;
   var Ackermann = WILHELM.Ackermann;
